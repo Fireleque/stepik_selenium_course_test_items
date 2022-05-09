39e2629b110145307cb472c71c82a181c80c4d74
Fireleque <vicesun@gmail.com>
2022-05-09 19:46:12 +0300

    lesson 3.6 step 9 - find basket button

---
+++ test_items.py
@@ -0,0 +1,9 @@
+import time
+
+link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_guest_should_see_es_basket_button(browser):
+    browser.get(link)
+    # time.sleep(30) # time.sleep for check language
+    button_add_to_basket = browser.find_elements_by_css_selector("button.btn-add-to-baske")
+    assert button_add_to_basket, 'Basket button not found'
